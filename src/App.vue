d800247989a0c4024a1912dc18b71aab63a28d6e
Rustam S. <blakfoh@gmail.com>
2025-07-06 17:35:10 +0300

    added scss in project

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import '../src/styles/index.scss'
+</script>
 
 <template>
 	<div class="wrapper-main">
 		<router-view />
 	</div>
 </template>
-
-<style>
-.wrapper-main {
-	width: 100%;
-	height: 100vh;
-	background-color: #f1f1f1;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: 24px;
-}
-</style>
--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,22 +1,11 @@
 <script setup lang="ts">
+import Bubble from '../components/Bubble.vue'
 import TextField from '../components/TextField.vue'
 </script>
 
 <template>
 	<div class="wrapper">
-		<TextField />
+		<Bubble />
 		<TextField />
 	</div>
 </template>
-
-<style scoped>
-.wrapper {
-	height: 100vh;
-	background-color: #cecece;
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-end;
-	gap: 24px;
-	padding: 40px 24px;
-}
-</style>
