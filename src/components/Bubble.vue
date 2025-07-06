ea3fe7daad87114768a4eb01759a95da3843b079
Rustam S. <blakfoh@gmail.com>
2025-07-06 17:44:58 +0300

    update Bubble.vue and _bubble.scss

--- src/components/Bubble.vue
+++ src/components/Bubble.vue
@@ -2,7 +2,12 @@
 
 <template>
 	<div class="bubble-wrapper">
-		<div class="bubble-avatar"></div>
+		<div class="bubble-avatar">
+			<img
+				src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+				alt=""
+			/>
+		</div>
 		<div class="bubble-message">
 			<div class="bubble-text">
 				Просто пишу, узнать как дела?Просто пишу, узнать как дела?Просто пишу,
