67ec5ff9582060a1c671930d5432aa5c59cc399c
Rustam S. <blakfoh@gmail.com>
2025-07-12 14:52:51 +0300

    feat: Show&Hide Profile, update Profile.vue and _profile.scss

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { getAuth, signOut } from 'firebase/auth'
 import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
 import logOutIcon from '../assets/logout.svg'
 import router from '../router/router'
 import { useUserStore } from '../store/user.ts'
@@ -18,22 +19,27 @@ const handleSignOut = async () => {
 		console.error('Error signing out:', error)
 	}
 }
+
+const isVisible = ref(false)
+
+function showProfile() {
+	isVisible.value = !isVisible.value
+}
 </script>
 
 <template>
-	<div class="profile-wrapper">
+	<div class="profile-wrapper" v-show="isVisible">
 		<div class="profile-data">
-			<div class="profile-avatar">
-				<img
-					:src="user?.photoURL || undefined"
-					:alt="user?.displayName || 'User Avatar'"
-					v-if="user"
-				/>
-			</div>
 			<div class="profile-info">
 				<h1 class="profile-name" v-if="user">{{ user.displayName }}</h1>
-				<p class="profile-email" v-if="user"><b>Email:</b> {{ user.email }}</p>
+				<p class="profile-email" v-if="user">{{ user.email }}</p>
 			</div>
+			<img
+				class="profile-avatar"
+				:src="user?.photoURL || undefined"
+				:alt="user?.displayName || 'User Avatar'"
+				v-if="user"
+			/>
 		</div>
 		<button class="profile-sign-out-button" @click="handleSignOut" v-if="user">
 			<slot></slot>
@@ -41,4 +47,11 @@ const handleSignOut = async () => {
 			<div>Выйти</div>
 		</button>
 	</div>
+	<img
+		@click="showProfile"
+		class="profile-avatar profile-avatar-btn"
+		:src="user?.photoURL || undefined"
+		:alt="user?.displayName || 'User Avatar'"
+		v-if="user"
+	/>
 </template>
